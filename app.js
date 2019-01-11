49472f4c0ceca19c3ba1e718b3651a6d78e62b70
SarunyaSWS <sarunyasws@gmail.com>
2019-01-11 16:03:23 +0700

    test

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 var testapp = new Vue ({
-el: '#testapp',
-    data : {
-        text: '',
-        age:10,
-        food:[],
-        gender:'',
-        selected:'',
-        status:true
-    },
-    methods : {
-        statuss:function()
-        {
-            this.status = !this.status
+    el: '#testapp',
+        data : {
+            text: '',
+            age:10,
+            food:[],
+            gender:'',
+            selected:'',
+            status:true
+        },
+        methods : {
+            statuss:function()
+            {
+                this.status = !this.status
+            }
         }
-    }
-   
+       
+        
     
-
-
-})
+    
+    })
